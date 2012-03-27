f7adcfea458e390871031d0731389bbb3fc371fb
Leon Dewey <leon@leondewey.com>
2012-03-27 22:51:49 +1100

    update

--- web.js
+++ web.js
@@ -6,17 +6,17 @@ var app = express.createServer(express.logger());
 
 app.get('/*', function(request, response) {
 
-  // console.log("request.params: " + );
-  // response.send('hi');
-
   var url = request.params;
   var path = new Number(Math.random() * 100000000000).toFixed(0) + '.jpg';
   var size = '1024x768';
 
   var to_exec = './bin/phantomjs/bin/phantomjs rasterize.js '+ url +' '+ path + ' ' + size;
+  //var to_exec = 'phantomjs rasterize.js '+ url +' '+ path + ' ' + size;
 
   exec(to_exec, function (error, stdout, stderr) {
-    if(error) response.send(error);
+    if(error) {
+      response.send(error + " <br />params: " + request.params + '<br />' + to_exec);
+    }
     else response.sendfile(path);
   });
 });
