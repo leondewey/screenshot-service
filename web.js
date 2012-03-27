b183f5eb8c7d0686b8978deef0044b0db32f0b53
Leon Dewey <leon@leondewey.com>
2012-03-27 22:43:54 +1100

    update

--- web.js
+++ web.js
@@ -4,11 +4,14 @@ var exec = require('child_process').exec;
 
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
+app.get('/*', function(request, response) {
 
-  var url = 'http://google.com';
+  // console.log("request.params: " + );
+  // response.send('hi');
+
+  var url = request.params;
   var path = new Number(Math.random() * 100000000000).toFixed(0) + '.jpg';
-  var size = '800x600';
+  var size = '1024x768';
 
   var to_exec = './bin/phantomjs/bin/phantomjs rasterize.js '+ url +' '+ path + ' ' + size;
 
